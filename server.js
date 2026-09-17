d5595e8295cb2b671cb75ef82cb7fde1d0edd6d7
Golam Masum Basar <gmbasr@gmail.com>
2026-09-18 01:31:58 +0530

    solved Render IPv4 issue

--- server.js
+++ server.js
@@ -80,7 +80,8 @@ const mailer = process.env.SMTP_HOST ? nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT || 587),
     secure: Number(process.env.SMTP_PORT) === 465,
-    auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASSWORD }
+    auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASSWORD },
+    family: 4
 }) : null;
 if (mailer) {
     mailer.verify()
